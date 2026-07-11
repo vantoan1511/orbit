3d1f2a100da80e2b987930bf848eb7dbc1933876
Toan Nguyen <vantoan1517@gmail.com>
2026-07-11 11:31:15 +0700

    chore: update neutralino config and clean up app test call

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { filesystem } from './services/nativeService'
 import AppLayout from './components/layout/AppLayout.vue'
 
 onMounted(() => {
   // Initialize dark mode by default
   document.documentElement.classList.add('my-app-dark')
   document.documentElement.setAttribute('data-theme', 'dark')
-
-  filesystem
-    .readDirectory('./')
-    .then((data) => {
-      console.log('Root directory contents:', data)
-    })
-    .catch((err) => {
-      console.error('Failed to read root directory:', err)
-    })
 })
 </script>
 
